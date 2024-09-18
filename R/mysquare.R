b632acd5abc71f6f5dd8192c9ddbc354162af8ef
Ethan Nguyen <ethannguyen419@gmail.com>
2024-09-18 13:10:56 -0500

    initial commit

---
+++ R/mysquare.R
@@ -0,0 +1,12 @@
+#' The Square Function
+#'
+#' @param x  # vector of quantitative data
+#'
+#' @return # vector
+#' @export
+#'
+#' @examples
+#' mysquare(x = 1:10)
+mysquare <- function(x){
+  x^2
+}
